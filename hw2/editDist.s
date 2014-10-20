dfdefca31dd86872c2b9f2ae14844217c006a4db
Jvlythical <lvlichael8@gmail.com>
2014-10-19 18:15:03 -0700

    Tab spacing fix

--- hw2/editDist.s
+++ hw2/editDist.s
@@ -1,6 +1,6 @@
 .data
-	string1:		.string		"test"
-	string2:		.string		"pest"
+	string1:	.string		"test"
+	string2:	.string		"pest"
 
 .text
 
@@ -8,32 +8,32 @@
 
 strlen:
 	push	%rbp
-	mov		%rsp, %rbp
+	mov		%rsp,	%rbp
 	sub		$0x8,	%rsp
-	movl 	$0x0, -0x4(%rbp)
-	movb	$0x0, %al
+	movl 	$0x0,	-0x4(%rbp)
+	movb	$0x0,	 %al
 
 	strlen_loop:	
-		sub		-0x4(%rbp), %edi		
+		sub		-0x4(%rbp),	%edi		
 		incl	-0x4(%rbp)
 		
-		movb	(%edi), %ah
-		cmp		%al, %ah
+		movb	(%edi),	%ah
+		cmp		%al,	%ah
 		jne		strlen_loop
 
-	mov	-0x4(%rbp), %eax	#Return string length
+	mov	-0x4(%rbp),	%eax	#Return string length
 
 	leaveq
 	retq
 
 main:	
 	push	%rbp
-	mov		%rsp, %rbp
+	mov		%rsp,	%rbp
 
-	mov 	$string1,	%edi		#Move address of string one to edi
+	mov 	$string1,	%edi	#Move address of string one to edi
 	callq	strlen
 
-	mov		$0x0, %eax 
+	mov		$0x0,	%eax 
 
 	leaveq
 	retq
