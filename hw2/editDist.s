affcacf043dc14b913e60ae6e492cd34b9679443
Jvlythical <lvlichael8@gmail.com>
2014-10-31 20:24:13 -0700

    Runnable rough draft

--- hw2/editDist.s
+++ hw2/editDist.s
@@ -8,18 +8,18 @@
 
 #min(int x, int y)
 min:
-	push 	%ebp
-	mov 	%esp, %ebp
+	pushq 	%rbp
+	mov 	%rsp, %rbp
 
 	cmp 	%edi, %edx
 	jl  	ret_x
 
 	ret_y:
-		mov 	%edx, %eax
-		jump 	min_ret
+		mov 	%edi, %eax
+		jmp 	min_ret
 
 	ret_x:
-		mov 	%edi, %eax
+		mov 	%edx, %eax
 		jmp 	min_ret
 
 	min_ret:
@@ -28,7 +28,7 @@ min:
 
 #int strlen(*str)
 strlen:
-	push 	%rbp
+	pushq 	%rbp
 	mov 	%rsp, %rbp
 	sub 	$0x8, %rsp		
 	
@@ -60,62 +60,65 @@ strlen:
 
 #int edit_dist(*string1, *string2)
 edit_dist:
-	push	%ebp
-	mov 	%esp, %ebp
+	pushq	%rbp
+	mov 	%rsp, %rbp
 
 #variable declarations
-	mov		%edi, -0x8(%ebp)	# *string1
-	mov		%edx, -0xc(%ebp)	# *string2
-	mov		$0x0, -0x10(%ebp)	# int strlen1
-	mov		$0x0, -0x14(%ebp)	# int strlen2
-	mov		$0x1, -0x18(%ebp)	# int i
-	mov		$0x1, -0x1c(%ebp)	# int n
-	mov		$0x0, -0x20(%ebp)	# int x
-	mov		$0x0, -0x24(%ebp)	# int y
-	mov		$0x0, -0x28(%ebp)	# int z
-	mov		$0x0, -0x2c(%ebp)	# *mat_addr
+	mov		%edi, -0x8(%rbp)	# *string1
+	mov		%edx, -0xc(%rbp)	# *string2
+	movl	$0x0, -0x10(%rbp)	# int strlen1
+	movl	$0x0, -0x14(%rbp)	# int strlen2
+	movl	$0x1, -0x18(%rbp)	# int i
+	movl	$0x1, -0x1c(%rbp)	# int n
+	movl	$0x0, -0x20(%rbp)	# int x
+	movl	$0x0, -0x24(%rbp)	# int y
+	movl	$0x0, -0x28(%rbp)	# int z
+	movl	$0x0, -0x2c(%rbp)	# *mat_addr
 
 #get values of string  lengths
 	callq	strlen
-	mov 	%eax, -0x10(%ebp)
-	mov		-0xc(%ebp), %edi
+	mov 	%eax, -0x10(%rbp)
+	mov		-0xc(%rbp), %edi
 	callq 	strlen
-	mov 	%eax, -0x14(%ebp)
+	mov 	%eax, -0x14(%rbp)
 
 #get mat_addr value	
 	inc 	%eax
-	mov 	-0x10(%ebp), %ebx
+	mov 	-0x10(%rbp), %ebx
 	inc 	%ebx
 	mul 	%ebx	
 	mov 	$0x4, %ebx
 	mul 	%ebx
-	mov 	%ebp, %ebx
-	sub 	%eax, %ebx
-	subl 	0x2c, %ebx
-	mov		%ebx, -0x2c(%ebp)
+	mov 	%rbp, %rbx
+	mov 	%eax, -0x40(%rbp)
+	sub 	-0x40(%rbp), %rbx
+	sub 	$0x2c, %rbx
+	mov		%rbx, -0x2c(%rbp)
 
 #for(int i = 0; i < strlen1.length + 1; i++)
-	mov 	-0x18(%ebp), %eax
-	mov 	$0x0, %ebx
-	mov 	-0x10(%ebp), %ecx
+	mov 	-0x18(%rbp), %ebx
+	mov 	$0x0, %eax
+	mov 	-0x10(%rbp), %ecx
+	mul 	%ecx
 	inc 	%ecx
 
 	str1_for:
-		mov 	%ebx, (%eax, %ebx, %ecx)
+		
+		mov 	%ebx, (%ebx, %eax, 1)
 
 		inc 	%ebx
 		cmp 	%ebx, %ecx
-		jlt 	str1_for
+		jl  	str1_for
 	
 	mov 	$0x0, %ebx
-	mov 	-0x14(%ebp), %ecx
+	mov 	-0x14(%rbp), %ecx
 
 	str2_for:
 		mov 	%ebx, (%eax, %ebx, 1)
 
 		inc 	%ebx
 		cmp 	%ebx, %ecx
-		jlt 	str2_for
+		jl  	str2_for
 
 
 #for(int i = 1; i < strlen1; i++)
@@ -123,79 +126,89 @@ edit_dist:
 
 	#for(int n = 1; n < strlen2; n++)
 		ed_str2_for:
-			mov 	-0x2c(%ebp), %ebx
+			mov 	-0x2c(%rbp), %ebx
 
 		# x = M[i-1][n] + 1
-			mov 	-0x18(%ebp), %eax
+			mov 	-0x18(%rbp), %eax
 			dec 	%eax
-			mul 	-0x10(%ebp)
-			add 	-0x1c(%bp), %eax
+			mov 	-0x10(%rbp), %edx
+			mul 	%edx
+			add 	-0x1c(%rbp), %eax
 			inc 	%eax
 			mov 	(%ebx, %eax, 1), %ecx
-			mov 	%ecx, -0x20(%ebp)
+			mov 	%ecx, -0x20(%rbp)
 
 		# y = M[a][b-1] + 1
-			mov 	-0x18(%ebp), %eax
-			mul 	-0x14(%ebp)
-			add 	-0x1c(%ebp), %eax
+			mov 	-0x18(%rbp), %eax
+			mov 	-0x10(%rbp), %edx
+			mul 	%edx
+			add 	-0x1c(%rbp), %eax
 			dec 	%eax
 			mov 	(%ebx, %eax, 1), %ecx
-			mov 	%ecx, -0x24(%ebp)
+			mov 	%ecx, -0x24(%rbp)
 	
 		# z = M[a-1][b-1] + (A[a-1] == B[b-1] ? 0 : 2)
-			mov 	-0x18(%ebp), %eax
-			mul 	-0x14(%ebp)
+			mov 	-0x18(%rbp), %eax
+			mov 	-0x10(%rbp), %edx
+			mul 	%edx
 			dec 	%eax
-			add 	-0x1c(%ebp), %eax
+			add 	-0x1c(%rbp), %eax
 			dec 	%eax
 			mov 	(%ebx, %eax, 1), %ecx
-			mov 	%ecx, -0x28(%ebp)
+			mov 	%ecx, -0x28(%rbp)
 
-			mov 	-0x18(%ebp), %eax
+			mov 	-0x18(%rbp), %eax
 			dec 	%eax
-			mov 	-0x8(%ebp), %ebx
+			mov 	-0x8(%rbp), %ebx
 			mov 	(%ebx, %eax, 1), %ecx
 			and 	$0x000000ff, %ecx 
 
-			mov 	-0x1c(%ebp), %eax
+			mov 	-0x1c(%rbp), %eax
 			dec 	%eax
-			mov 	-0xc($ebp), %eax
+			mov 	-0xc(%rbp), %eax
 			mov 	(%ebx, %eax, 1), %edx
 
-			mov 	-0x28(%ebp), %edi
+			mov 	-0x28(%rbp), %edi
 			cmp 	%ecx, %edx
 			je  	min
 
 			add 	$0x2, %edi
-			mov 	-0x24(%ebp), %edx
+			mov 	-0x24(%rbp), %edx
 			callq 	min
 
 			mov 	%eax, %edi
-			mov 	-0x20(%ebp), %edx
+			mov 	-0x20(%rbp), %edx
 			callq 	min
 
 			mov 	%eax, %edx
 
-			mov 	-0x18(%ebp), %eax
-			mul 	-0x14(%ebp)
-			add 	-0x1c(%ebp), %eax
+			mov 	-0x18(%rbp), %eax
+			mov 	-0x14(%rbp), %ecx
+			mul 	%ecx
+			add 	-0x1c(%rbp), %eax
 			mov 	%edx, (%ebx, %eax, 1) 
 
 		#n--
-			dec -0x1c(%rbp)
-			cmp	-0x1c(%rbp), strlen2
-			jl	ed_str2_for
+			decl 	-0x1c(%rbp)
+			mov 	-0x14(%rbp), %esi
+			cmp 	-0x1c(%rbp), %esi 
+			jl  	ed_str2_for
 		
 	#i--
-		dec	-0x18(%rbp)
-		cmp	-0x18(%ebp), strlen1
-		jl	ed_str1_for
+		mov  	-0x10(%rbp), %edx
+		decl 	-0x18(%rbp)
+		mov 	-0x10(%rbp), %esi
+		cmp 	-0x18(%rbp), %esi
+		jl  	ed_str1_for
 
 	ret_dist:
-		mov 	-0x2c(%ebp), %ebx
-		mov 	-0x10(%ebp), %eax
-		mul 	-0x14(%ebp)
-		add 	-0x14(%ebp), %eax
+		mov 	-0x2c(%rbp), %ebx
+		mov 	-0x10(%rbp), %eax
+		
+		mov 	-0x14(%rbp), %edx
+		mul 	%edx
+		
+		add 	-0x14(%rbp), %eax
 		mov 	(%ebx, %eax, 1), %ecx
 		mov 	%ecx, %eax
 
@@ -204,11 +217,12 @@ edit_dist:
 
 #main()
 main:	
-	push	%rbp
+	pushq	%rbp
 	mov		%rsp,	%rbp
 
 	mov 	$string1,	%edi	#Move address of string one to edi
-	callq	strlen
+	mov 	$string2, 	%edx
+	callq	edit_dist
 
 	mov		$0x0,	%eax 
 
